e98c82b11dc6c94df41935214d7f16f56783c376
javierechiburu <javierechiburu.g@gmail.com>
2024-03-06 13:23:44 -0300

    layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,7 +3,12 @@
 import SideNav from '@/components/Sidenav';
 import useUser from '@/hooks/useUser';
 
-export default function Layout(props: { children: React.ReactNode, modal: React.ReactNode }) {
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
     const user = useUser();
     return (
       (user && <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-gray-200">
@@ -11,8 +16,7 @@ export default function Layout(props: { children: React.ReactNode, modal: React.
       <div className="w-full flex-none md:w-64 z-10">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12 z-10">{props.children}</div>
-      {props.modal}
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12 z-10">{children}</div>
       <div className='absolute bg-yellow-300 w-full h-32 rounded-b-lg'></div>
     </div>)
     );
